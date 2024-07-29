ce7d3acebfbf2346bbe84d0b920abc69f82c2a78
Changeryto <changeryto@hotmail.com>
2024-07-29 16:49:17 -0600

    Se añade integración con conda, creando su propio entorno.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,5 +6,10 @@ git init
 git add .
 git commit -m "Commit inicial del proyecto {{ cookiecutter.project_title }}"
 echo "Repositorio inicializado correctamente."
-codium . || code . || echo "No ha instalado code o codium."
+
+conda env create --file environment.yml
+conda activate {{ cookiecutter.project_slug }}
+codium . || code . || echo "No ha instalado VScode o VScodium."
+echo "Ambiente de conda creado con exito."
+
 exit 0
