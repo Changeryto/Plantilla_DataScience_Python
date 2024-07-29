ff4e57689ad5cb3fb12376ca96ba1fe0b63e63ef
Changeryto <changeryto@hotmail.com>
2024-07-29 16:28:54 -0600

    Se aniade el uso de codium o code.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,4 +6,5 @@ git init
 git add .
 git commit -m "Commit inicial del proyecto {{ cookiecutter.project_title }}"
 echo "Repositorio inicializado correctamente."
+codium . || code . || echo "No ha instalado code o codium."
 exit 0
