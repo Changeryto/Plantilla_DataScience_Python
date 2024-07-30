730b5ed2a46902f54a732bc0091102eace71582d
Changeryto <changeryto@hotmail.com>
2024-07-29 22:23:41 -0600

    Se cambia la ejecucion de conda.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,9 +7,8 @@ git add .
 git commit -m "Commit inicial del proyecto {{ cookiecutter.project_title }}"
 echo "Repositorio inicializado correctamente."
 
-conda env create --file environment.yml
+conda env create --file environment.yml && echo "Ambiente de conda creado con exito."
 conda activate {{ cookiecutter.project_slug }}
 codium . || code . || echo "No ha instalado VScode o VScodium."
-echo "Ambiente de conda creado con exito."
 
 exit 0
